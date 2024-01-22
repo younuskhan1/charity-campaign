a86796429f4574fb99f06942a5def51fcb6fd252
younuskhan1 <md.younuskhan412@gmail.com>
2024-01-22 23:59:22 +0600

    cannot complete the target

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,32 @@
 import "./Navbar.css";
-import image from "../../../public/Logo.png";
-import { NavLink } from "react-router-dom";
+// import image from "../../../public/Logo.png";
+// import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="navbar-parent">
-           <img className="icon-image" src={image} alt="" />
+        <div>
+            <input type="checkbox" id="check" />
+            <label htmlFor ="check" className="checkBtn">
+              <i className="fa-solid fa-bars"></i>
+            </label>
+           <nav>
+            <label className="logo">DesignX</label>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Donation</a></li>
+                <li><a href="#">Statistics</a></li>
+            </ul>
+           </nav>
+        </div>
+    );
+};
+
+export default Navbar;
+
+
+
+
+{/* <img className="icon-image" src={image} alt="" />
            <div className="nav-bar-links">
               <NavLink to = "/" 
               className={({ isActive, isPending }) =>
@@ -17,9 +38,4 @@ const Navbar = () => {
               <NavLink to = "/statistics"  className={({ isActive, isPending }) =>
               isPending ? "pending" : isActive ? "active" : ""
              }>Statistics</NavLink>
-           </div>
-        </div>
-    );
-};
-
-export default Navbar;
+           </div> */}
