18558e8ace49bccf90ab46242e6db0111c56a4f8
younuskhan1 <md.younuskhan412@gmail.com>
2024-01-28 14:58:54 +0600

    input field validation has been done with sweet alert

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,21 +3,37 @@ import Banner from "../Banner/Banner";
 import "./Home.css";
 import Card from "../Card/Card";
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 const Home = () => {
     
     const [categoryWiseData, setCategoryWiseData] = useState([]);
 
     const donations = useLoaderData();
-    
+
     const handleSearchButton = ()=> {
         // console.log("just clicked the search button.");
         const searchInputField = document.getElementById("input-field");
         const inputFieldValue = searchInputField.value;
         searchInputField.value = "";
         // console.log(inputFieldValue);
-        const categoryData = donations?.filter((item)=>(item.Category).toLowerCase() === inputFieldValue.toLowerCase());
-        setCategoryWiseData(categoryData);
+        if(inputFieldValue === ""){
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Please input me health or education or clothing or food. Do not keep me empty!",
+              });
+        }else if(!isNaN(inputFieldValue)){
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Please input me health or education or clothing or food",
+              });
+        }
+        else{
+            const categoryData = donations?.filter((item)=>(item.Category).toLowerCase() === inputFieldValue.toLowerCase());
+            setCategoryWiseData(categoryData);
+        } 
     }
 
     // console.log(donations);
