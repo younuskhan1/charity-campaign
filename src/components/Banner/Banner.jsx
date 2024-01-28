c12cfce96235d6e205cc252b35464c35ee047bc8
younuskhan1 <md.younuskhan412@gmail.com>
2024-01-28 12:35:40 +0600

    search button functionality is done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,16 +1,21 @@
-
+import PropTypes from 'prop-types'; // ES6
 import "./Banner.css";
 
-const Banner = () => {
+const Banner = ({handleSearchButton}) => {
     return (
         <div className="banner-parent ">
             <div className="background-image"></div> 
             <div className="banner-heading-parent">
                 <p className="banner-heading">I Grow By Helping People In Need</p>
-                <p className="input-button-parent"><input className="search-input-field" type="text" placeholder="Search here....."/><button className="search-button">Search</button></p>
+                <p className="input-button-parent">
+                <input id='input-field' className="search-input-field" type="text" placeholder="Search here....."/>
+                <button className="search-button" onClick={handleSearchButton}>Search</button></p>
             </div>
         </div>
     );
 };
 
+Banner.propTypes ={
+    handleSearchButton:PropTypes.func.isRequired,
+}
 export default Banner;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,16 +2,30 @@ import { useLoaderData } from "react-router-dom";
 import Banner from "../Banner/Banner";
 import "./Home.css";
 import Card from "../Card/Card";
+import { useState } from "react";
 
 const Home = () => {
+    
+    const [categoryWiseData, setCategoryWiseData] = useState([]);
+
     const donations = useLoaderData();
+    const handleSearchButton = ()=> {
+        // console.log("just clicked the search button.");
+        const searchInputField = document.getElementById("input-field");
+        const inputFieldValue = searchInputField.value;
+        searchInputField.value = "";
+        // console.log(inputFieldValue);
+        const categoryData = donations?.filter((item)=>(item.Category).toLowerCase() === inputFieldValue.toLowerCase());
+        setCategoryWiseData(categoryData);
+    }
+
     // console.log(donations);
     return (
         <div className="home-parent">
-            <Banner></Banner>
+            <Banner handleSearchButton = {handleSearchButton}></Banner>
             <div className="cards-parent">
                {
-
+                categoryWiseData?.length ? categoryWiseData?.map((donate,index) => <Card key={index} donate={donate}></Card>) :
                 donations?.map((donate, index) => <Card key={index} donate = {donate}></Card>)
                
                }
