804de64b9cfdb36412a98d3755b67bb848279c19
younuskhan1 <md.younuskhan412@gmail.com>
2024-01-29 14:58:58 +0600

    some more reviews are added

--- src/components/NoDonationsSelected/NoDonationsSelected.jsx
+++ src/components/NoDonationsSelected/NoDonationsSelected.jsx
@@ -4,7 +4,7 @@ const NoDonationsSelected = () => {
     return (
         <div>
             <div className="no-donation-selected">
-                <h1>You have donated nothing yet...</h1>
+                <p className="no-donation-heading">You have donated nothing yet...</p>
                 <div className="go-to-home-button-div"> <Link to = "/"><button className="go-to-home">Go to Home</button></Link> </div>
             </div>
         </div>
